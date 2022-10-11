8e1579630be0feb7487100afabc67e65f56979cc
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-10-11 23:07:46 +0200

    move models and app code into according files

--- dav_material/app.py
+++ dav_material/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
