bc883845de1617e0da0dafff513939bfc8452348
josephgrabinger <joseph.grabinger@yahoo.de>
2022-10-16 16:38:07 +0200

    add mock data

---
+++ frontend/lib/common/models/mockData/mock_data_material.dart
@@ -0,0 +1,154 @@
+import 'package:frontend/common/models/material.dart';
+
+
+final EquipmentType mockRopeEquipmentType = EquipmentType(
+  id: 1,
+  description: 'Seil',
+);
+
+final EquipmentType mockHelmetEquipmentType = EquipmentType(
+  id: 2,
+  description: 'Helm',
+);
+
+final EquipmentType mockCarbineEquipmentType = EquipmentType(
+  id: 3,
+  description: 'Karabiner',
+);
+
+
+final Property mockLengthProperty = Property(
+  id: 1,
+  name: 'Länge',
+  description: 'Länge des Seils',
+  value: '10',
+  unit: 'm',
+);
+
+final Property mockThicknessProperty = Property(
+  id: 2,
+  name: 'Dicke',
+  description: 'Dicke des Seils',
+  value: '3',
+  unit: 'cm',
+);
+
+final Property mockSizeProperty = Property(
+  id: 3,
+  name: 'Größe',
+  description: 'Größe des Helms',
+  value: '55',
+  unit: 'cm',
+);
+
+
+final PurchaseDetails mockPurchaseDetails = PurchaseDetails(
+  id: 1,
+  purchaseDate: DateTime(2021, 1, 1),
+  invoiceNumber: '123456',
+  merchant: 'Kletterladen',
+  productionDate: DateTime(2020, 1, 1),
+  purchasePrice: 10,
+  suggestedRetailPrice: 20,
+);
+
+
+final SerialNumber mockSerialNumber = SerialNumber(
+  id: 1,
+  manufacturer: 'Kletter-Stuff XY',
+);
+
+
+final List<Material> mockMterial = [
+  Material(
+    id: 1,
+    serialNumbers: [
+      mockSerialNumber,
+    ],
+    inventoryNumber: '235vh2354-2',
+    maxLifeExpectancy: '10 years',
+    maxServiceDuration: '10 years',
+    installationDate: DateTime(2021, 1, 1),
+    instructions: 'Use with care',
+    nextInspectionDate: DateTime(2022, 2, 1),
+    rentalFee: 5,
+    condition: Condition.good,
+    usage: 4,
+    purchaseDetails: mockPurchaseDetails,
+    equipmentTypes: [
+      mockRopeEquipmentType,
+    ],
+    properties: [
+      mockLengthProperty, 
+      mockThicknessProperty,
+    ],
+  ),
+  Material(
+    id: 2,
+    serialNumbers: [
+      mockSerialNumber,
+    ],
+    inventoryNumber: '235vh2354-3',
+    maxLifeExpectancy: '10 years',
+    maxServiceDuration: '10 years',
+    installationDate: DateTime(2021, 1, 1),
+    instructions: 'Use with care',
+    nextInspectionDate: DateTime(2022, 2, 1),
+    rentalFee: 7,
+    condition: Condition.good,
+    usage: 4,
+    purchaseDetails: mockPurchaseDetails,
+    equipmentTypes: [
+      mockHelmetEquipmentType,
+    ],
+    properties: [
+      mockSizeProperty,
+    ],
+  ),
+  Material(
+    id: 3,
+    serialNumbers: [
+      mockSerialNumber,
+    ],
+    inventoryNumber: 'cg135vh2354-3',
+    maxLifeExpectancy: '20 years',
+    maxServiceDuration: '10 years',
+    installationDate: DateTime(2019, 1, 1),
+    instructions: 'Use with care',
+    nextInspectionDate: DateTime(2022, 2, 1),
+    rentalFee: 2,
+    condition: Condition.good,
+    usage: 4,
+    purchaseDetails: mockPurchaseDetails,
+    equipmentTypes: [
+      mockCarbineEquipmentType,
+    ],
+    properties: [
+      mockThicknessProperty,
+    ],
+  ),
+  Material(
+    id: 4,
+    serialNumbers: [
+      mockSerialNumber,
+    ],
+    inventoryNumber: 'q35vh2fc4-2',
+    maxLifeExpectancy: '10 years',
+    maxServiceDuration: '10 years',
+    installationDate: DateTime(2021, 1, 1),
+    instructions: 'Use with care',
+    nextInspectionDate: DateTime(2022, 6, 1),
+    rentalFee: 5,
+    condition: Condition.broken,
+    usage: 8,
+    purchaseDetails: mockPurchaseDetails,
+    equipmentTypes: [
+      mockRopeEquipmentType,
+    ],
+    properties: [
+      mockLengthProperty, 
+      mockThicknessProperty,
+    ],
+  ),
+];
+
---
+++ frontend/lib/common/models/mockData/mock_data_user.dart
@@ -0,0 +1,136 @@
+import 'package:frontend/common/models/user.dart';
+
+
+final Right mockAdministrationRight = Right(
+  id: 1,
+  name: 'Administration',
+  description: 'Can access and edit the administration page',
+);
+
+final Right mockLenderRight = Right(
+  id: 2,
+  name: 'Lender',
+  description: 'Can access the lender page adn lend material',
+);
+
+final Right mockInspectionRight = Right(
+  id: 3,
+  name: 'Inspection',
+  description: 'Can do inspections',
+);
+
+final Right mockInventoryRight = Right(
+  id: 4,
+  name: 'Inventory',
+  description: 'Can access and edit the inventory page',
+);
+
+
+final Role mockBasicRole = Role(
+  id: 1,
+  name: 'Basic',
+  description: 'Basic user without any right',
+  rights: [],
+);
+
+final Role mockIncpectorRole = Role(
+  id: 2,
+  name: 'Inspector',
+  description: 'Can do inspections',
+  rights: [
+    mockInspectionRight,
+    mockInventoryRight,
+  ],
+);
+
+final Role mockInstructorRole = Role(
+  id: 3,
+  name: 'Instructor',
+  description: 'Can access everything besides inspections and administration',
+  rights: [
+    mockLenderRight,
+    mockInventoryRight,
+  ],
+);
+
+final Role mockAdministratiorRole = Role(
+  id: 4,
+  name: 'Administrator',
+  description: 'Can manage User and Role administration',
+  rights: [
+    mockAdministrationRight,
+  ],
+);
+
+
+final List<User> mockUsers = [
+  User(
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@gmail.com',
+    phone: '+49123456789',
+    membershipNumber: 123456,
+    address: Address(
+      street: 'Musterstraße',
+      houseNumber: 1,
+      city: 'Musterstadt',
+      zip: '12345',
+    ),
+    roles: [
+      mockBasicRole,
+    ],
+  ),
+  User(
+    id: 2,
+    firstName: 'Tim',
+    lastName: 'Doe',
+    email: 'tim.doe@gmail.com',
+    phone: '+4914345569',
+    membershipNumber: 1235432,
+    address: Address(
+      street: 'Musterweg',
+      houseNumber: 12,
+      city: 'Musterhausen',
+      zip: '54321',
+    ),
+    roles: [
+      mockInstructorRole,
+    ],
+  ),
+  User(
+    id: 3,
+    firstName: 'Sarah',
+    lastName: 'Koe',
+    email: 'sarah.koe@gmail.com',
+    phone: '+4914341234229',
+    membershipNumber: 998432,
+    address: Address(
+      street: 'Musterweg',
+      houseNumber: 2,
+      city: 'Musterhausen',
+      zip: '54321',
+    ),
+    roles: [
+      mockIncpectorRole,
+      mockAdministratiorRole,
+    ],
+  ),
+  User(
+    id: 4,
+    firstName: 'Klara',
+    lastName: 'Koe',
+    email: 'klara.koe@gmail.com',
+    phone: '+4914333332129',
+    membershipNumber: 091234,
+    address: Address(
+      street: 'Klosterstraße',
+      houseNumber: 26,
+      city: 'Musterhausen',
+      zip: '54321',
+    ),
+    roles: [
+      mockAdministratiorRole,
+    ],
+  ),
+];
