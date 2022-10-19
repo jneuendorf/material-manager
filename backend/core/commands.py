f1b9ceadfcc641018e43122d4a22599d597a496b
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-10-20 00:28:56 +0200

    [backend] user test data

--- backend/core/commands.py
+++ backend/core/commands.py
@@ -1,3 +1,6 @@
+from importlib import import_module
+
+import click
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,7 +8,12 @@ from flask_sqlalchemy import SQLAlchemy
 class Commands:
     def __init__(self, app: Flask, db: SQLAlchemy):
         @app.cli.command("create-db")
-        # @click.argument("name")
         def create_db():
             with app.app_context():
                 db.create_all()
+
+        @app.cli.command("create-test-data")
+        @click.argument("extension")
+        def create_test_data(extension: str):
+            with app.app_context():
+                import_module(f"extensions.{extension}.fixtures")
