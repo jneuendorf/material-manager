5a352121302b815ec55106c7d15a31df0c0da389
Aiman Al-Hazmi <aiman.alhazmi@fu-berlin.de>
2022-11-07 15:05:12 +0100

    Ressourcen anlegen- first

---
+++ backend/extensions/inspection/resources.py
@@ -0,0 +1,54 @@
+from typing import List
+
+from flask import abort
+from flask_apispec import use_kwargs
+from marshmallow import fields
+from sqlalchemy.exc import IntegrityError
+from sympy import field
+
+from core.helpers.resource import ModelListResource, ModelResource
+from core.helpers.schema import BaseSchema, ModelConverter
+
+from . import models
+
+class CommentSchema(BaseSchema):
+    class Meta:
+        model = models.Comment
+        fields = (
+            "id",
+            "inspection_id",
+            "comment",
+            "photo"
+        )
+    
+
+class Comment(ModelResource):
+    
+    Schema = CommentSchema
+    
+    
+class InspectionSchema(BaseSchema):
+    class Meta:
+        model = models.Inspection
+        fields = (
+            "id", 
+            "inspector_id",
+            "material_id",
+            "date",
+            "type",
+            
+        )
+    
+    
+class Inspection(ModelResource):
+    rul = [
+         "/inspection",
+        "/inspection/<int:inspection_id>"
+    ]
+    Schema = InspectionSchema
+    
+    def get(self, inspection_id: int):
+        pass
+    
+    def post(self, inspection_id: int):
+        pass
