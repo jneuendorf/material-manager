44e724ccc96385268794e59b22c18293a7bd648f
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-11-07 19:47:54 +0100

    [backend] inspection type as enum, comment as text, fix photo string length

--- backend/extensions/inspection/models.py
+++ backend/extensions/inspection/models.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Type
 
 from core.extensions import db
@@ -6,12 +7,17 @@ from core.helpers.orm import CrudModel
 Model: Type[CrudModel] = db.Model
 
 
+class InspectionType(enum.Enum):
+    NORMAL = "NORMAL"  # Sichtprüfung
+    PSA = "PSA"
+
+
 class Inspection(Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     inspector_id = db.Column(db.ForeignKey("user.id"))
     material_id = db.Column(db.ForeignKey("material.id"))
     date = db.Column(db.Date)
-    type = db.Column(db.String)  # PSA- / Sichtprüfung
+    type = db.Column(db.Enum(InspectionType, create_constraint=True))
     comments = db.relationship(
         "Comment",
         backref="inspection",
@@ -20,6 +26,6 @@ class Inspection(Model):  # type: ignore
 
 class Comment(Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.String)
-    photo = db.Column(db.String)  # URL
+    comment = db.Column(db.Text)
+    photo = db.Column(db.String(length=128))  # URL
     inspection_id = db.Column(db.ForeignKey(Inspection.id))
