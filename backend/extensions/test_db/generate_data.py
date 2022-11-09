b986665edb867f1004a10d7a57f5032aa1af7154
Dupon <jonathan.st@hotmail.de>
2022-11-09 13:34:17 +0100

    generate deterministic, remove all random

--- backend/extensions/test_db/generate_data.py
+++ backend/extensions/test_db/generate_data.py
@@ -1,5 +1,4 @@
 import datetime
-import random
 
 from extensions.material.models import (
     Condition,
@@ -12,16 +11,6 @@ from extensions.material.models import (
 )
 
 
-def rand_date(start_yyyy, start_mm, start_dd, end_yyyy, end_mm, end_dd):
-    start_dt = datetime.date(start_yyyy, start_mm, start_dd)
-    end_dt = datetime.date(end_yyyy, end_mm, end_dd)
-    time_between_dates = end_dt - start_dt
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_dt + datetime.timedelta(days=random_number_of_days)
-    return random_date
-
-
 def test_data():
     i = 1
     inventory_identifier = ["J", "K", "L", "G"]
@@ -30,14 +19,8 @@ def test_data():
     set_names = ["bergtour", "eiswand", "wandern", "zelten"]
     material_types = ["gold", "silber", "bronze", "holz"]
     manufacturers = ["edelrid", "the blue light", "elliot", "black diamont"]
-
-    pro_ID = Property.get_or_create(
-        id=1,
-        name="TODO",
-        description="hier sollte eine Beschreibung stehen",
-        value=random.randint(0, 99),
-        unit=random.choice(units),
-    )
+    years_future = [2023, 2024, 2025, 2026]
+    years_past = [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014]
 
     for j in material_types:
         mat_ID = MaterialType.get_or_create(
@@ -54,46 +37,58 @@ def test_data():
 
     while i <= 200:
 
-        rand_inventory_num = random.sample(inventory_identifier, k=2)
-        rand_inventory_num = (
-            rand_inventory_num[0] + rand_inventory_num[1] + str(random.randint(0, 99))
-        )
-
         if i % 50 == 0:
-            rand_condition = Condition.BROKEN
+            new_condition = Condition.BROKEN
         elif i % 20 == 0:
-            rand_condition = Condition.REPAIR
+            new_condition = Condition.REPAIR
         else:
-            rand_condition = Condition.OK
+            new_condition = Condition.OK
+
+        if i <= 20:
+            pro_ID = Property.get_or_create(
+                id=i,
+                name="TODO",
+                description="hier sollte eine Beschreibung stehen",
+                value=i * i % 100,
+                unit=units[i % 3],
+            )
 
         ser_ID = SerialNumber.get_or_create(
             id=i,
             serial_number=i,
-            production_date=rand_date(2014, 1, 1, 2022, 1, 1),
-            manufacturer=random.choice(manufacturers),
+            production_date=datetime.date(
+                years_past[i * 2 % 7], i * 3 % 12 + 1, i * 4 % 29 + 1
+            ),
+            manufacturer=manufacturers[i % 3],
             material_id=i,
         )
 
         pur_ID = PurchaseDetails.get_or_create(
             id=i,
-            purchase_date=rand_date(2014, 1, 1, 2022, 9, 1),
-            invoice_number=random.choice(inventory_identifier) + str(i * 5),
-            merchant=random.choice(merchants),
-            purchase_price=round(random.uniform(0.01, 99.99), 2),
-            suggested_retail_price=round(random.uniform(0.01, 99.99), 2),
+            purchase_date=datetime.date(
+                years_past[i * 3 % 7], i * 2 % 12 + 1, i * 5 % 29 + 1
+            ),
+            invoice_number=inventory_identifier[i % 3] + str(i * 5),
+            merchant=merchants[i % 3],
+            purchase_price=float(i * 5 % 100) + float(0.01 * i % 100),
+            suggested_retail_price=float(i * 3 % 100) + float(0.01 * i % 80),
         )
 
         Material.get_or_create(
             id=i,
-            inventory_number=rand_inventory_num,
-            max_life_expectancy=random.randint(0, 9),
-            max_service_duration=random.randint(0, 9),
-            installation_date=rand_date(2014, 1, 1, 2022, 9, 1),
-            instructions="Drei Knoten ohne Hände im Kopfstand",
-            next_inspection_date=rand_date(2022, 9, 1, 2023, 6, 1),
-            rental_fee=round(random.uniform(0.01, 99.99), 2),
-            condition=rand_condition.name,
-            days_used=random.randint(0, 99),
+            inventory_number=str(inventory_identifier[i % 3]) + str(i * 5 % 100),
+            max_life_expectancy=i % 8,
+            max_service_duration=i % 10,
+            installation_date=datetime.date(
+                years_past[i * 4 % 7], i * 3 % 12 + 1, i * 2 % 29 + 1
+            ),
+            instructions="hier sollte eine gebrauchsanweisung stehen",
+            next_inspection_date=datetime.date(
+                years_future[i * 2 % 3], i * 3 % 12 + 1, i * 4 % 29 + 1
+            ),
+            rental_fee=float(i * 2 % 40) + float(0.01 * i % 100),
+            condition=new_condition,
+            days_used=i * 5 % 100,
             # many to one (FK here)
             # material_type_id = db.Column(db.ForeignKey(MaterialType.id))
             # many to one (FK here)
