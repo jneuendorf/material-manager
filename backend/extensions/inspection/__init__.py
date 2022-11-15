3976d91ec86826f5a541e0820c864f4913f810cd
Lars <larlan102@gmail.com>
2022-11-15 21:03:10 +0100

    fix for pipeline linting error

--- backend/extensions/inspection/__init__.py
+++ backend/extensions/inspection/__init__.py
@@ -1,16 +1,16 @@
 from core.helpers.extension import Extension
 
 from . import models, resources
+
 inspection = Extension(
     "inspection",
     __name__,
     models=(
         models.Inspection,
-     #   models.Comment,
+        models.Comment,
     ),
     resources=(
         resources.Inspection,
-        # resources.Comment,
-        resources.Comments
-        ),
+        resources.Comments,
+    ),
 )
