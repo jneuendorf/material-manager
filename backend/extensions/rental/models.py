690237e9bec47cdcffbe920990bf31ba3c0d73a9
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-11-15 11:47:30 +0100

    [backend] fix rental model relations and specify column null/defaults

--- backend/extensions/rental/models.py
+++ backend/extensions/rental/models.py
@@ -1,4 +1,5 @@
 import enum
+from datetime import datetime
 from typing import Type
 
 from sqlalchemy import Table
@@ -18,31 +19,48 @@ class RentalStatus(enum.Enum):
 
 
 class Rental(Model):  # type: ignore
-
-    __tablename__ = (
-        "rental"  # funktioniert hier aus irgendeinem Grund. Liegt nicht am Import
-    )
-
     id = db.Column(db.Integer, primary_key=True)
+    # many to one (FK here)
     customer_id = db.Column(db.ForeignKey("user.id"))
+    customer = db.relationship(
+        "User",
+        # foreign_keys=[db.Column("rental.customer_id")],
+        backref="customer_rentals",
+        primaryjoin="Rental.customer_id == User.id",
+        uselist=False,
+    )
+    # many to one (FK here)
     lender_id = db.Column(db.ForeignKey("user.id"))
-    # rental_status_id = db.Column(db.ForeignKey(RentalStatus.id))
-    cost = db.Column(db.Float)
-    discount = db.Column(db.Float)
-    deposit = db.Column(db.Float)  # Kaution
-    created_at = db.Column(db.DateTime)
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
-    usage_start_date = db.Column(db.Date)
-    usage_end_date = db.Column(db.Date)
+    lender = db.relationship(
+        "User",
+        backref="lender_rentals",
+        primaryjoin="Rental.lender_id == User.id",
+        uselist=False,
+    )
+    # many to one (FK here)
     return_to_id = db.Column(db.ForeignKey("user.id"))
+    return_to = db.relationship(
+        "User",
+        backref="receiver_rentals",
+        primaryjoin="Rental.return_to_id == User.id",
+        uselist=False,
+    )
     # one to many (FK on child)
     return_infos = db.relationship("ReturnInfo", backref="rental")
+
     rental_status = db.Column(
         db.Enum(RentalStatus, create_constraint=True),
         nullable=False,
         default=RentalStatus.AVAILABLE,
     )
+    cost = db.Column(db.Float, nullable=False)
+    discount = db.Column(db.Float, default=0)
+    deposit = db.Column(db.Float, default=0)  # Kaution
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now())
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    usage_start_date = db.Column(db.Date)
+    usage_end_date = db.Column(db.Date)
 
 
 class ReturnInfo(Model):  # type: ignore
