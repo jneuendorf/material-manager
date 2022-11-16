5f7753e60654b1f5039b8ac8c61d9a3f4506485c
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-11-16 23:49:03 +0100

    [backend] make wsgi have an app attr

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,8 @@
-if __name__ == "__main__":
-    from app import create_app
-    from core.config import flask_config
-    from core.extensions import db, mail
+from app import create_app
+from core.config import flask_config
+from core.extensions import db, mail
 
-    app = create_app(flask_config, db, mail, drop_db=False)
+app = create_app(flask_config, db, mail, drop_db=False)
+
+if __name__ == "__main__":
     app.run()
