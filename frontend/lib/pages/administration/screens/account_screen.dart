317df551a5e2f6a823216985dd4517dde52e8785
josephgrabinger <joseph.grabinger@yahoo.de>
2022-11-16 13:38:19 +0100

    [frontend] fix role filter

--- frontend/lib/pages/administration/screens/account_screen.dart
+++ frontend/lib/pages/administration/screens/account_screen.dart
@@ -27,7 +27,7 @@ class AccountScreen extends StatelessWidget {
               title: 'roles'.tr,
               options: [
                 'all'.tr,
-                ...administrationPageController.userController.permissions.map((e) => e.name)
+                ...administrationPageController.userController.roles.map((e) => e.name)
               ],
               selected: administrationPageController.selectedFilter.value?.name ?? 'all'.tr,
               onSelected: administrationPageController.onFilterSelected,
