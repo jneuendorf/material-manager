0507eeb4b88e878a2e24fd1b0a9295a628481919
Dupon <jonathan.st@hotmail.de>
2022-11-21 20:17:53 +0100

    resolve threads (cycle, float)

--- backend/extensions/rental/sample_data.py
+++ backend/extensions/rental/sample_data.py
@@ -1,12 +1,16 @@
+import itertools
 from datetime import date, timedelta
 
 from extensions.material.models import Material
 from extensions.rental.models import Rental, RentalStatus
 
-NUM_Rental = 50
-materials = []
+NUM_RENTAL = 50
 
-for i in range(NUM_Rental):
+material_list = Material.all()
+assert material_list, "Cannot create rentals because there are no materials"
+materials = itertools.cycle(material_list)
+
+for i in range(NUM_RENTAL):
     if i % 50 == 0:
         rentalStatus = RentalStatus.RETURNED
     elif i % 40 == 0:
@@ -16,19 +20,18 @@ for i in range(NUM_Rental):
     else:
         rentalStatus = RentalStatus.AVAILABLE
 
-    materials = [Material.get(id=i + 1), Material.get(id=(i % 10) + 2)]
     Rental.get_or_create(
         customer_id=i,
         lender_id=i,
         rental_status=rentalStatus,
-        cost=float(i % 100) + float((i % 100) * 0.01),
+        cost=i % 100 + (i % 100) * 0.01,
         discount=int(i % 100),
-        deposit=3 * (float(i % 100) + float((i % 100) * 0.01)),
+        deposit=3 * (i % 100) + (i % 100) * 0.01,
         created_at=date(2022, 1, 1) + timedelta(days=i),
         start_date=date(2022, 1, 1) + timedelta(days=i + 7),
         end_date=date(2022, 1, 1) + timedelta(days=i + 14),
         usage_start_date=None,
         usage_end_date=None,
         return_to_id=i,
-        _related=dict(materials=materials),
+        _related=dict(materials=[next(materials), next(materials)]),
     )
